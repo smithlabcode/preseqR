569e531e8830db0008d9ce9afa8374793fd29a6a
Jake <chaodeng@usc.edu>
2014-08-08 20:07:21 -0700

    add regex matching; os for path

--- add_preseq_src.py
+++ add_preseq_src.py
@@ -1,42 +1,63 @@
 #!/usr/bin/python
+
+### automatically copy and modify required files from preseq to preseqR
+### change header file names from '*hpp' to '*.h'
+### change ' #include "header.hpp" ' to ' #include "header.h" ' in src files
+
 import sys
 import shutil
-# set the path for preseq directory
-if len(sys.argv) == 2:
-	src_path = sys.argv[1]
-else:
-	sys.stderr.write('Please specify the path of preseq!\n')
-	sys.exit(1)
-# src_path is the path of preseq
-src_path = src_path.strip()
-if src_path[-1] != '/':
-	src_path += '/'
-# set the file names
-cppfile = 'continued_fraction.cpp'
-headerfile = 'continued_fraction.hpp'
-# des_path is the path of preseqR/src
-des_path = './src/'
-# copy files
-shutil.copy(src_path + cppfile, des_path + cppfile)
-shutil.copy(src_path + headerfile, des_path + 'continued_fraction.h')
-# read all lines
-f = open(des_path + cppfile, 'r')
-lines = f.readlines()
-f.close()
-# replace the line '#include "continued_fraction.hpp"' with "continued_fraction.h"'
-f = open(des_path + cppfile, 'w')
-# set the keywords to target the line
-keywords = ('#include', '"continued_fraction.hpp"', '#include"continued_fraction.cpp"')
-for line in lines:
-	words = line.strip().split()
-	# check whether the line is #include "continued_fraction.hpp"
-	if keywords[0] in words and keywords[1] in words:
-		f.write('#include "continued_fraction.h"\n')
-	# no space between #include and "continued_fraction.hpp"
-	elif keywords[2] in words:
-		f.write('#include "continued_fraction.h"\n')
-	else:
-		f.write(line)
+import os
+import fileinput
+import re
 
-f.close()
+## set the paths for preseq and preseqR directory
+if len(sys.argv) == 3:
+  preseqPath = sys.argv[1]
+  preseqRPath = sys.argv[2]
+else:
+  sys.stderr.write('Usage: ./add_preseq_src.py [preseq path] [preseqR path]\n')
+  sys.exit(1)
+
+## check any file including keyWords in its name
+## matchingFile contains file names with keywords 
+filenames = os.listdir(preseqPath)
+keyWords = "continued_fraction"
+matchingFile = [i for i in filenames if keyWords in i]
+
+## copy required files from preseq into preseqR/src directory
+preseqRSrcPath = os.path.join(preseqRPath, 'src')
+for i in matchingFile:
+  srcFile = os.path.join(preseqPath, i)
+  desFile = os.path.join(preseqRSrcPath, i)
+  shutil.copy(srcFile, desFile)
+
+## change header suffix from 'hpp' with 'h'
+os.chdir(preseqRSrcPath)
+filenames = os.listdir('./')
+headerFeature = re.compile('.+\.hpp')
+headerFile = []
+for i in filenames:
+
+  ## check whether a file is a header file with '.hpp'
+  if headerFeature.match(i):
+    os.rename(i, i.replace('.hpp', '.h'))
+    headerFile.append(i)
+
+## modify #include statement since the name of the header changes
+## #include "*.hpp" ----> #include "*.h"
+srcFeature = re.compile('.+\.cpp')
+lineFeature = [re.compile('\#include.*' + i + '.*') for i in headerFile]
+
+for i in filenames:
+
+  ## check whether a file is a src file with '.cpp'
+  if srcFeature.match(i):
+    for line in fileinput.input(i, inplace = True):
+      
+      ## check whether src code include a header which has changed name
+      ## if does, rewrite the line to include the new header
+      if any(feature.match(line) for feature in lineFeature):
+        line = line.replace('.hpp', '.h')
+      sys.stdout.write(line)
+
 sys.exit(0)
