5a573cd8f827ab4dd8727d2663557d9fc8803305
chaodengusc <chaodeng@usc.edu>
2015-08-20 16:48:51 -0700

    estimating E(S_1(t)) / t instead of E(S_1(t))

--- R/mincount.R
+++ R/mincount.R
@@ -314,7 +314,8 @@ preseqR.rfa.mincount <- function(n, mt = 50, ss = NULL,
 
 
 ## species accum curves basis on parital fraction expansion estimation
-preseqR.pf.mincount <- function(n, mt = 100, ss = NULL, 
+## Rational function approximation to E(S_1(t))
+preseqR.pf.mincount.c0 <- function(n, mt = 100, ss = NULL, 
                                 max.extrapolation = NULL, r=1)
 {
   checking.hist(n)
@@ -712,3 +713,203 @@ preseqR.rfa.curve.derivSelect <- function(n, mt=100, ss=NULL,
   result <- list(continued.fraction = CF, estimates = yield.estimates)
   return(result)
 }
+
+
+## species accum curves basis on parital fraction expansion estimation
+## Rational function approximation to E(S_1(t)) / t instead of E(S_1(t))
+preseqR.pf.mincount <- function(n, mt = 100, ss = NULL, 
+                                     max.extrapolation = NULL, r=1)
+{
+  checking.hist(n)
+  ## setting the diagonal value
+  di = 0
+  ## minimum required number of terms of power series in order to construct
+  ## a continued fraction approximation
+  MIN_REQUIRED_TERMS <- 4
+
+  ## calculate total number of sample
+  total.sample <- n[, 1] %*% n[, 2]
+  total.sample <- floor(total.sample)
+
+  ## set step.size as the size of the initial experiment if it is undefined
+  if (is.null(ss)) {
+    ss <- floor(total.sample)
+    step.size <- ss
+  } else if (ss < 1) {
+    write("The step size is too small", stderr())
+    return()
+  } else {
+    step.size <- floor(ss)
+  }
+
+  ## no interpolation if step.size is larger than the size of experiment
+  ## set the starting sample size as the step.size
+  if (step.size > total.sample) {
+    yield.estimates <- vector(mode = 'numeric', length = 0)
+
+    ## starting sample size for extrapolation
+    starting.size <- step.size
+  } else {
+      ## interpolation when sample size is no more than total sample size
+      ## interpolate and set the size of the sample for an initial extrapolation
+      out <- preseqR.interpolate.mincount(step.size, n, r)
+      yield.estimates <- out[, 2]
+
+      ## starting sample size for extrapolation
+      starting.size <- ( floor(total.sample/step.size) + 1 )*step.size
+  }
+
+  if (is.null(max.extrapolation)) {
+    ## extrapolation 100 times if it is undefined; 100 is a magic number
+    max.extrapolation <- 100*total.sample
+  }
+
+  PS.coeffs <- generating.ps(n, mt)
+
+  ## only use power series with non-zero coefficients
+  ## effective coefficients from begining up to the first zero
+  counts.before.first.zero = 1
+  while (as.integer(counts.before.first.zero) <= length(PS.coeffs) &&
+         PS.coeffs[counts.before.first.zero] != 0)
+    counts.before.first.zero <- counts.before.first.zero + 1
+
+  ## constrain the continued fraction approximation with even degree 
+  ## conservatively estimates
+  mt <- min(mt + 1, counts.before.first.zero - 1)
+  mt <- mt - (mt %% 2)
+
+  ## pre-check to make sure the sample is good for prediction
+  if (mt < MIN_REQUIRED_TERMS)
+  {
+    m <- paste("max count before zero is les than min required count (4)",
+               " sample not sufficiently deep or duplicates removed", sep = ',')
+    write(m, stderr())
+    return(NULL)
+  }
+
+  PS.coeffs <- PS.coeffs[ 1:mt ]
+
+  ## construct a continued fraction approximation with 
+  ## maximum available degree
+  valid = FALSE
+  DE = seq(mt, 4, by=-2)
+  for (de in DE) {
+    out <- .C('c_PS2CF', as.integer(di), 
+              as.integer(de), as.double(PS.coeffs[1:de]), 
+              as.integer(length(PS.coeffs[1:de])),
+              ps.coeffs = as.double(vector(mode = 'numeric', length=MAXLENGTH)),
+              ps.coeffs.l = as.integer(0),
+              cf.coeffs = as.double(vector(mode = 'numeric', length=MAXLENGTH)),
+              cf.coeffs.l = as.integer(0),
+              offset.coeffs =as.double(vector(mode='numeric',length=MAXLENGTH)),
+              diagonal.idx = as.integer(0),
+              degree = as.integer(0), is.valid = as.integer(0));
+
+    if (out$is.valid) {
+      ## pass results into R variables
+      length(out$ps.coeffs) <- out$ps.coeffs.l
+      length(out$cf.coeffs) <- out$cf.coeffs.l
+      length(out$offset.coeffs) <- as.integer(abs(out$diagonal.idx))
+      CF <- list(out$ps.coeffs, out$cf.coeffs, out$offset.coeffs, 
+                 out$diagonal.idx, out$degree)
+      names(CF) <- c('ps.coeffs', 'cf.coeffs', 'offset.coeffs', 'diagonal.idx',
+                     'degree')
+      class(CF) <- 'CF'
+
+      ## compatible with constructing continued fraction in C extension
+      RF <- CF2RFA(CF)
+      RF[[1]] <- RF[[1]] / polynomial(c(0, 1))
+
+      numer.roots <- solve(RF[[1]])
+      denom.roots <- solve(RF[[2]])
+      ## seperating roots by their real parts
+      numer.roots.neg <- numer.roots[which(Re(numer.roots) < 0)]
+      numer.roots.pos <- numer.roots[which(Re(numer.roots) >= 0)]
+      denom.roots.neg <- denom.roots[which(Re(denom.roots) < 0)]
+      denom.roots.pos <- denom.roots[which(Re(denom.roots) >= 0)]
+
+      ## record roots in the numerator that are significantly similar to
+      ## roots in the denominator
+      tmp.roots <- c()
+
+      ## remove roots in both numerator and denominator if the real part >= 0 
+      ## and the difference is less than the predefined PRECISION
+      for (i in 1:length(numer.roots.pos)) {
+        if (length(denom.roots.pos) > 0) {
+          d <- Mod(denom.roots.pos - numer.roots.pos[i])
+          for (j in 1:length(d)) {
+            if (d[j] < PRECISION) {
+              denom.roots.pos <- denom.roots.pos[-j]
+              tmp.roots <- c(tmp.roots, numer.roots.pos[i])
+              break
+            }
+          }
+        }
+      }
+      numer.roots <- numer.roots[!numer.roots %in% tmp.roots]
+      denom.roots <- c(denom.roots.neg, denom.roots.pos)
+
+      ## assume 1 <= t <= max range
+      ## convert roots from t - 1 to t
+      ## max.range = max.extrapolation / total.sample
+      roots <- denom.roots + 1
+      if (length(which(roots == 0)) || length(which(Re(roots) > 0 & 
+          Mod(roots) / Re(roots) / 2 <= as.double(max.extrapolation / total.sample)))) {
+        next
+      } else {
+        valid = TRUE
+        poly.numer <- as.function(poly.from.roots(numer.roots))
+        l <- length(denom.roots)
+        ## treat polynomials in the rational function as monic
+        ## the difference is a constant C
+        coef <- sapply(1:l, function(x) {
+          poly.numer(denom.roots[x]) / prod(denom.roots[x] - denom.roots[-x])})
+        ## calculate the constant C
+        C <- coef(RF[[1]])[length(coef(RF[[1]]))] / 
+             coef(RF[[2]])[length(coef(RF[[2]]))]
+        ## species accum curves with minimum count r
+        ## using parital fraction expansion
+        f <- function(t) { 
+          Re(sapply(t, function(x) {coef %*% ( (x+1)/(x-denom.roots) )^r})) * C}
+        mincount.accum.curve.f <- function(x) {f(x - 1)}
+        break
+      }
+    }
+  }
+
+  if (valid == FALSE) {
+    return(NULL)
+  }
+
+  ## if the sample size is larger than max.extrapolation
+  ## stop extrapolation
+  ## MINOR.correction prevents machinary precision from biasing comparison
+  ## result
+  if (starting.size > (max.extrapolation + MINOR.correction))
+  {
+    index <- as.double(step.size) * (1:length(yield.estimates))
+    yield.estimates <- list(sample.size = index, yields = yield.estimates)
+    result <- list(continued.fraction = CF, yield.estimates = yield.estimates)
+    return(result)
+  }
+
+  ## extrapolation for experiment with large sample size
+  start <- starting.size / total.sample
+  end <- (max.extrapolation + MINOR.correction) / total.sample
+  step <- step.size / total.sample
+
+  ## extrapolating for the general accumulation curves
+  extrap <- mincount.accum.curve.f(seq(start, end, by=step))
+
+  ## combine results from interpolation/extrapolation
+  yield.estimates <- c(yield.estimates, extrap)
+  index <- as.double(step.size) * (1: length(yield.estimates))
+
+  ## put index and estimated yields together into a two-colunm matrix
+  yield.estimates <- matrix(c(index, yield.estimates), ncol = 2, byrow = FALSE)
+  colnames(yield.estimates) <- c('sample.size', 'yield.estimate')
+
+  result <- list(continued.fraction = CF, estimates = yield.estimates)
+  return(result)
+}
+
