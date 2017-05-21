a50de231a9df5cf6161f5faef64612e8da257c36
chaodengusc <chaodeng@usc.edu>
2017-05-21 15:09:31 -0700

    add a license

--- R/rational_function.R
+++ R/rational_function.R
@@ -1,3 +1,22 @@
+#    Copyright (C) 2016 University of Southern California and
+#             Chao Deng and Andrew D. Smith and Timothy Daley
+#
+#    Authors: Chao Deng
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 ## continued fraction approximant to a power series
 ## QD algorithm
 ## input: coefficients of the power series; begin with the constant
